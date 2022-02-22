468354110a7ced379eb5d1b03684b8100d579df9
njugunaObi <mbuthianjuguna18@gmail.com>
2022-02-22 15:26:46 +0300

    more commits

--- my_resume/settings.py
+++ my_resume/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'my_resume')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'views.main_page'),)
 django_heroku.settings(locals())
 
 # Default primary key field type
